b583199b1938ef6cce03ffe0ccc4b701c624bb85
sukso96100 <sukso96100@gmail.com>
2019-01-26 11:38:27 +0000

    updated crypto functions to use salt and iv for generating key

--- src/main/Utils.kt
+++ src/main/Utils.kt
@@ -5,7 +5,9 @@ import javax.crypto.spec.*
 import io.ktor.application.*
 import java.util.Formatter
 import io.ktor.util.hex
-import javax.crypto.*
+import java.security.SecureRandom
+import javax.crypto.spec.IvParameterSpec
+import java.io.ByteArrayOutputStream
 
 val emailRegex = Regex("^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,6}$")
 
@@ -15,16 +17,53 @@ fun hash(password: String): String {
     return hex(hmac.doFinal(password.toByteArray(Charsets.UTF_8)))
 }
 //https://www.baeldung.com/java-cipher-class
-fun encryptWithKey(keyBytes: ByteArray, vararg toEncrypt: String): List<ByteArray>{
+//https://stackoverflow.com/questions/44878997/handling-of-iv-and-salt-in-java-encryption-and-decryption
+//https://github.com/phxql/kotlin-crypto-example/blob/master/src/main/kotlin/de/mkammerer/Crypto.kt
+fun encryptWithKey(key: String, toEncrypt: String): ByteArray{
+    
+    // Generate salt
+    val secRandom = SecureRandom()
+    val salt = ByteArray(16)
+    secRandom.nextBytes(salt)
+    
+    // generate secret with key and salt
+    val secretKey = generateSecretKey(key, salt)
+    
     val cipher = Cipher.getInstance("AES/ECB/PKCS5Padding")
-    val secretKey = SecretKeySpec(keyBytes, "AES");
-    cipher.init(Cipher.ENCRYPT_MODE, secretKey);
-    return toEncrypt.map{ cipher.doFinal(it.toByteArray()) }
+    cipher.init(Cipher.ENCRYPT_MODE, secretKey)
+    
+    // get IV from cipher
+    val params = cipher.getParameters()
+    val iv = params.getParameterSpec(IvParameterSpec::class.java).getIV()
+    
+    // encrypt the data
+    val encrypted = cipher.doFinal(toEncrypt.toByteArray(Charsets.UTF_8))
+    val outputStream = ByteArrayOutputStream()
+    outputStream.write(salt)
+    outputStream.write(iv)
+    outputStream.write(encrypted)
+    return outputStream.toByteArray()
 }
 
-fun decryptWithKey(keyBytes: ByteArray, vararg toDecrypt: String): List<ByteArray>{
+fun decryptWithKey(key: String, toDecrypt: ByteArray): String{
+    // Cut out salt, iv and content from the encrypted data
+    val salt = toDecrypt.copyOfRange(0, 16)
+    val iv = toDecrypt.copyOfRange(16, 32)
+    val content = toDecrypt.copyOfRange(32, toDecrypt.size)
+    
+    // generate secret with key and salt
+    val secretKey = generateSecretKey(key, salt)
+    
     val cipher = Cipher.getInstance("AES/ECB/PKCS5Padding")
-    val secretKey = SecretKeySpec(keyBytes, "AES")
-    cipher.init(Cipher.DECRYPT_MODE, secretKey)
-    return toDecrypt.map{ cipher.doFinal(it.toByteArray()) }
+    cipher.init(Cipher.DECRYPT_MODE, secretKey, IvParameterSpec(iv))
+    val decrypted = cipher.doFinal(content)
+    return String(decrypted, Charsets.UTF_8)
+}
+
+fun generateSecretKey(key: String, salt: ByteArray): SecretKeySpec{
+    // generate secret with key and salt
+    val factory = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA1")
+    val spec = PBEKeySpec(key.toCharArray(), salt, 65536, 256)
+    val tmp = factory.generateSecret(spec)
+    return SecretKeySpec(tmp.getEncoded(), "AES")
 }

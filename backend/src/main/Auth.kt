ff0306c08dbaf31a8d60c9c3bdb28cd15665858a
sukso96100 <sukso96100@gmail.com>
2019-03-11 11:49:26 +0000

    send error code for signup api when returning error

--- backend/src/main/Auth.kt
+++ backend/src/main/Auth.kt
@@ -23,21 +23,21 @@ fun Route.auth(){
         // Sign Up Function
         post("/signup"){
             val params = call.receive<SignUpForm>()
-            val username = params.username ?: return@post call.respondText("username is empty", status = HttpStatusCode.BadRequest)
-            val email = params.email ?: return@post call.respondText("email is empty", status = HttpStatusCode.BadRequest)
-            val displayName = params.displayName ?: return@post call.respondText("display name is empty", status = HttpStatusCode.BadRequest)
-            val password = params.password ?: return@post call.respondText("password is empty", status = HttpStatusCode.BadRequest)
-            val passwordCheck = params.passwordCheck ?: return@post call.respondText("password check is empty", status = HttpStatusCode.BadRequest)
+            val username = params.username ?: return@post call.respond(HttpStatusCode.BadRequest, ResponseWithCode(0, "username is empty"))
+            val email = params.email ?: return@post call.respond(HttpStatusCode.BadRequest, ResponseWithCode(1, "email is empty"))
+            val displayName = params.displayName ?: return@post call.respond(HttpStatusCode.BadRequest, ResponseWithCode(2, "display name is empty"))
+            val password = params.password ?: return@post call.respond(HttpStatusCode.BadRequest, ResponseWithCode(3, "password is empty"))
+            val passwordCheck = params.passwordCheck ?: return@post call.respond(HttpStatusCode.BadRequest, ResponseWithCode(4, "password check is empty"))
             
             when {
                 // Validate sign up form
-                password.length < 8 -> call.respondText("Password must be at least 8 digits", status = HttpStatusCode.BadRequest)
-                password != passwordCheck -> call.respondText("You have to type same value for password and password check.", status = HttpStatusCode.BadRequest)
-                username.length < 4 -> call.respondText("Username must be longer then 4 letters", status = HttpStatusCode.BadRequest)
-                displayName.length < 3 -> call.respondText("Display name must be longer then 3 letters", status = HttpStatusCode.BadRequest)
-                !emailRegex.matches(email) -> call.respondText("Email address is not valid.", status = HttpStatusCode.BadRequest)
-                DbQueries.findByUsername(username) != null -> return@post call.respondText("Username ${username} is already in use", status = HttpStatusCode.BadRequest)
-                DbQueries.findByEmail(email) != null -> return@post call.respondText("Email ${email} is already in use", status = HttpStatusCode.BadRequest)
+                password.length < 8 -> call.respond(HttpStatusCode.BadRequest, ResponseWithCode(5, "Password must be at least 8 digits"))
+                password != passwordCheck -> call.respond(HttpStatusCode.BadRequest, ResponseWithCode(6, "You have to type same value for password and password check."))
+                username.length < 4 -> call.respond(HttpStatusCode.BadRequest, ResponseWithCode(7, "Username must be longer then 4 letters"))
+                displayName.length < 3 -> call.respond(HttpStatusCode.BadRequest, ResponseWithCode(8, "Display name must be longer then 3 letters"))
+                !emailRegex.matches(email) -> call.respond(HttpStatusCode.BadRequest, ResponseWithCode(9, "Email address is not valid."))
+                DbQueries.findByUsername(username) != null -> return@post call.respond(HttpStatusCode.BadRequest, ResponseWithCode(10, "Username ${username} is already in use"))
+                DbQueries.findByEmail(email) != null -> return@post call.respond(HttpStatusCode.BadRequest, ResponseWithCode(11, "Email ${email} is already in use"))
                 else -> {
                     // Sign up form validated! create new user with the form data
                     val passwordHash = BCrypt.hashpw(password, BCrypt.gensalt())

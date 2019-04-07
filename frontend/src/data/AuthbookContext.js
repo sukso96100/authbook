bbf62fd36ef28cd2f25f116a5571e8c33ef06b71
sukso96100 <sukso96100@gmail.com>
2019-04-07 06:47:03 +0000

    Let AuthbookContextProvider to manage context data

--- frontend/src/data/AuthbookContext.js
+++ frontend/src/data/AuthbookContext.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 export const ContextDefaults = {
     userinfo: {
@@ -22,3 +22,63 @@ export const ContextDefaults = {
 };
 
 export const AuthbookContext = React.createContext(ContextDefaults);
+
+export class AuthbookContextProvider extends Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            ...ContextDefaults,
+            setUserinfo: this.setUserinfo.bind(this),
+            setEncryptionKey: this.setEncryptionKey.bind(this),
+            setAccounts: this.setAccounts.bind(this),
+            addAccount: this.addAccount.bind(this),
+            updateAccount: this.updateAccount.bind(this),
+            deleteAccount: this.deleteAccount.bind(this)
+        };
+    }
+    
+    setUserinfo(data){ 
+        this.setState({userinfo: data});
+    }
+
+    setEncryptionKey(data){
+        this.setState({encryptionKey: data});
+    }
+
+    setAccounts(data){ 
+        this.setState({accounts: data});
+    }
+    
+    addAccount(data){ 
+        this.setState({
+            accounts: [...this.state.accounts, data]
+        });
+    }
+
+    updateAccount(index, data){ 
+        let tmp = this.state.accounts;
+        tmp[index] = {
+            ...tmp[index],
+            seedName: data.seedName,
+            url: data.url,
+            accountUserName: data.accountUserName,
+            seedInfo: data.seedInfo
+        };
+        if(data.otpKey) tmp[index].otpKey = data.otpKey;
+        this.setState({accounts: tmp}); 
+    }
+
+    deleteAccount(index){ 
+        let tmpArr = this.state.accounts;
+        tmpArr.splice(index, 1);
+        this.setState({accounts: tmpArr});
+    }
+    
+    render(){
+        return(
+            <AuthbookContext.Provider value={this.state}>
+                  {this.props.children}
+            </AuthbookContext.Provider>
+        )
+    }
+}
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -6,69 +6,20 @@ import * as serviceWorker from './serviceWorker';
 import App from './App';
 import Login from './auth/Login';
 import history from './history';
-import {AuthbookContext, ContextDefaults} from './data/AuthbookContext';
+import {AuthbookContextProvider} from './data/AuthbookContext';
 
-
-export class Root extends Component{
-    
-    constructor(props){
-        super(props);
-        this.state = {
-            ...ContextDefaults,
-            setUserinfo: this.setUserinfo.bind(this),
-            setEncryptionKey: this.setEncryptionKey.bind(this),
-            setAccounts: this.setAccounts.bind(this),
-            addAccount: this.addAccount.bind(this),
-            updateAccount: this.updateAccount.bind(this),
-            deleteAccount: this.deleteAccount.bind(this)
-        };
-    }
-    
-    setUserinfo(data){ this.setState({userinfo: data});}
-
-    setEncryptionKey(data){ this.setState({encryptionKey: data});}
-
-    setAccounts(data){ this.setState({accounts: data});}
-    
-    addAccount(data){ this.setState({accounts: [...this.state.accounts, data]});}
-
-    updateAccount(index, data){ 
-        let tmp = this.state.accounts;
-        tmp[index] = {
-            ...tmp[index],
-            seedName: data.seedName,
-            url: data.url,
-            accountUserName: data.accountUserName,
-            seedInfo: data.seedInfo
-        };
-        if(data.otpKey) tmp[index].otpKey = data.otpKey;
-        this.setState({accounts: tmp}); }
-
-    deleteAccount(index){ 
-        let tmpArr = this.state.accounts;
-        tmpArr.splice(index, 1);
-        this.setState({accounts: tmpArr});
-    }
-    
-    
-    render(){
-        return(
-            <AuthbookContext.Provider value={this.state}>
-                  <Router history={history}>
-                      <div>
-                            <Route exact path="/" render={() => (
-                                <Redirect to="/app/home"/>
-                            )}/>
-                            <Route path="/app" component={App} />
-                            <Route exact path="/login" component={Login}/>
-                      </div>
-                  </Router>
-            </AuthbookContext.Provider>
-        )
-    }
-}
 ReactDOM.render((
-<Root></Root>
+    <AuthbookContextProvider>
+        <Router history={history}>
+            <div>
+                <Route exact path="/" render={() => (
+                        <Redirect to="/app/home"/>
+                    )}/>
+                <Route path="/app" component={App} />
+                <Route exact path="/login" component={Login}/>
+            </div>
+        </Router>
+    </AuthbookContextProvider>
 ), document.getElementById('root'));
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
